c52d44d4a83b4e803943635766dadbb21ef7429a
iMac <imac@iMacs-iMac.local>
2017-07-30 20:48:09 -0500

    adding up roundScore

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+var roundScore = 0; 
+
 document.querySelector(".roll-dice").addEventListener("click", function () {
 	var dice = Math.floor(Math.random() * 6) + 1; 
-
+	console.log(dice)
+	
 	document.querySelector(".dice").src = 'dice-' + dice + '.png'; 
-});
+
+	roundScore += dice; 
+	document.querySelector(".current-1").textContent = roundScore; 
+	console.log(roundScore); 
+	});
 
 
