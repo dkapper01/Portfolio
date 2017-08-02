254950b37785549014059fe0d3bce0e1a8dfedd0
dkapper01 <dkapper01@gmail.com>
2017-08-02 18:00:12 -0500

    catch up

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+
+
 document.querySelector(".roll-dice").addEventListener("click", function () {
 	var dice = Math.floor(Math.random() * 6) + 1; 
 
 	document.querySelector(".dice").src = 'dice-' + dice + '.png'; 
 });
-
 
