8f49a29217774190b5fafca32ea275a781dae454
dkapper01 <dkapper01@gmail.com>
2017-08-02 18:33:10 -0500

    add score

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-<<<<<<< HEAD
 
-
-=======
 var roundScore = 0; 
 var totalScore = 0; 
-<<<<<<< HEAD
->>>>>>> f4e362a15a6ac359f9205e4ffc544311b2e47b3f
-=======
 
->>>>>>> 9a9316b64fa6c9cfbb2635fd6fbf39977b73e542
+document.querySelector('.current-1').textContent = '0';
+document.querySelector('.score').textContent = '0'; 
+
 document.querySelector(".roll-dice").addEventListener("click", function () {
 	var dice = Math.floor(Math.random() * 6) + 1; 
 	console.log(dice)
@@ -20,14 +16,13 @@ document.querySelector(".roll-dice").addEventListener("click", function () {
 	console.log(roundScore); 
 });
 
-<<<<<<< HEAD
-=======
 
 document.querySelector('.hold-dice').addEventListener('click', function () {
-	totalScore = roundScore; 
+	totalScore += roundScore; 
 
 	document.querySelector('.score').textContent = totalScore; 
 
+	roundScore = 0;
+	document.querySelector('.current-1').textContent = '0'; 
+}); 
 
-})
->>>>>>> f4e362a15a6ac359f9205e4ffc544311b2e47b3f
