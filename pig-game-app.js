8adcedaa05fc4abcf6453693eb88fb5259eb78d4
dkapper01 <dkapper01@gmail.com>
2017-08-11 22:46:44 -0500

    new js file

---
+++ pig-game-app.js
@@ -0,0 +1,83 @@
+init(); 
+var roundScore = 0; 
+var totalScore = 0;
+var activePlayer = 0; 
+var totalScore = [0,0]; 
+var gamePlaying = true; 
+
+
+
+document.querySelector(".btn-roll-dice").addEventListener("click", function () {
+	var dice = Math.floor(Math.random() * 6) + 1; 
+	
+	
+	document.querySelector(".dice").src = 'img/dice-' + dice + '.png'; 
+	console.log(dice); 
+
+	if(dice != 1) {
+		roundScore += dice; 
+		document.querySelector('.current-' + activePlayer).textContent = roundScore; 
+	} else {
+		roundScore = 0;
+		// next player 
+		console.log('next player'); 
+
+		nextPlayer(); 
+	}
+	
+});
+
+
+document.querySelector('.btn-hold-dice').addEventListener('click', function () {
+	
+		totalScore[activePlayer] += roundScore; 
+
+		document.getElementById('score-' + activePlayer).textContent = totalScore[activePlayer]; 
+		
+		document.querySelector('.current-' + activePlayer).textContent = '0';   
+
+		if(totalScore[activePlayer] >= 20) {
+			document.querySelector('.title-' + activePlayer).innerHTML = '<h1>Winner!</h1>'
+
+			if(activePlayer == 0) {
+				document.querySelector('.title-1').innerHTML = '<h1>Loser!</h1>'
+				document.querySelector('.player-1-panel').classList.remove('active');
+
+
+			} else {
+				document.querySelector('.title-0').innerHTML = '<h1>Loser!</h1>'
+				document.querySelector('.player-1-panel').classList.remove('active');
+
+			}
+
+		}
+
+		nextPlayer();
+}); 
+
+document.querySelector(".btn-new-game").addEventListener("click", init); 
+
+function init () { 
+
+	gamePlaying = true; 
+	document.querySelector('.current-0').textContent = '0';
+	document.getElementById('score-0').textContent = '0';
+
+	document.querySelector('.current-1').textContent = '0';
+	document.getElementById('score-1').textContent = '0';
+	document.querySelector('.player-0-panel').classList.add('active');
+	document.querySelector('.player-1-panel').classList.remove('active');
+}
+
+function nextPlayer () {
+	roundScore = 0
+	if(activePlayer === 0) {
+		activePlayer = 1; 
+		document.querySelector('.player-0-panel').classList.remove('active');
+		document.querySelector('.player-1-panel').classList.add('active');
+	} else {
+		activePlayer = 0; 
+		document.querySelector('.player-1-panel').classList.add('active');
+		document.querySelector('.player-1-panel').classList.remove('.active');
+	}
+}
