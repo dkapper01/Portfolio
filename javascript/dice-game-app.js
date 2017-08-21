e6172a22d762160c12d1df3dfac971d57d02590f
dkapper01 <dkapper01@gmail.com>
2017-08-21 10:48:00 -0500

    new play btn that starts game

--- javascript/dice-game-app.js
+++ javascript/dice-game-app.js
@@ -14,11 +14,32 @@ document.querySelector(".roll-btn").addEventListener("click", function() {
 
 }); 
 
+document.querySelector('.play-btn').addEventListener('click', function () {
 
+	document.querySelector('.play-btn').style.display = 'none'; 
+	
+	document.querySelector(".dice-0").style.display = 'block';
+	document.querySelector('.dice-1').style.display = 'block'; 
+	document.querySelector('.start-btn').style.display = 'block'; 
+	document.querySelector('.roll-btn').style.display = 'block'; 
+	document.querySelector('.hold-btn').style.display = 'block';  
+
+	document.querySelector('.player-score-0').textContent = "0"; 
+	document.querySelector('.current-score-0').textContent = "0"; 
+
+	document.querySelector('.player-score-1').textContent = '0'; 
+	document.querySelector('.current-score-1').textContent = '0'; 
+
+	document.querySelector('.player-name-0').classList.add('bg-success'); 
+	document.querySelector('.player-name-1').classList.add('bg-danger');
+
+});
 
 function init () {
+
 	document.querySelector(".dice-0").style.display = 'none';
 	document.querySelector('.dice-1').style.display = 'none'; 
+	document.querySelector('.start-btn').style.display = 'none'; 
 	document.querySelector('.roll-btn').style.display = 'none'; 
 	document.querySelector('.hold-btn').style.display = 'none';  
 
