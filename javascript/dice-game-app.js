4c738ebb4fb3450bf673e9845282517e2c411a69
dkapper01 <dkapper01@gmail.com>
2017-08-21 19:19:12 -0500

    working dice game

--- javascript/dice-game-app.js
+++ javascript/dice-game-app.js
@@ -1,23 +1,97 @@
 init(); 
+var score = [0,0]; 
+var roundScore = 0; 
+var activePlayer = 0; 
 
+document.querySelector(".roll-btn").addEventListener("click", function() { 
 
- 
-
-
-document.querySelector(".roll-btn").addEventListener("click", function() {
-	console.log("daniel"); 
 	var diceOne = Math.floor(Math.random() * 6) + 1; 
 	var diceTwo = Math.floor(Math.random() * 6) + 1;
 
 	document.querySelector(".dice-0").src = "img/dice-" + diceOne + ".png";
 	document.querySelector(".dice-1").src = "img/dice-" + diceTwo + ".png"; 
 
+
+	if(diceOne !== 1 && diceTwo !== 1) {
+		roundScore += diceOne + diceTwo; 
+		document.getElementById('current-' + activePlayer).textContent = roundScore; 
+
+	} else { 
+		document.getElementById('current-' + activePlayer).textContent = '0';
+
+	nextPlayer(); 
+
+	}
 }); 
+
+document.querySelector('.hold-btn').addEventListener('click', function () {
+
+	score[activePlayer] += roundScore;
+	document.getElementById('score-' + activePlayer).textContent = score[activePlayer]; 
+	document.getElementById('current-' + activePlayer).textContent = '0'; 
+
+	if(score[activePlayer] >= 100) {
+		document.querySelector('.panel-' + activePlayer).textContent = 'Winner!'; 
+
+		if(activePlayer === 1) {
+			document.querySelector('.panel-0').textContent = 'Loser!';
+		} else {
+			document.querySelector('.panel-1').textContent = 'Loser!';
+		}
+	} 
+	 
+	nextPlayer(); 
+});
 
 document.querySelector('.play-btn').addEventListener('click', starteGame); 
 document.querySelector('.start-btn').addEventListener('click', starteGame)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function nextPlayer () {
+	roundScore = 0
+
+	if(activePlayer === 0) {
+		activePlayer = 1; 
+	} else {
+		activePlayer = 0
+	}
+}
+
 function starteGame () {
+	score = [0,0]
+	roundScore = 0; 
 	document.querySelector('.play-btn').style.display = 'none'; 
 
 	document.querySelector(".dice-0").style.display = 'block';
